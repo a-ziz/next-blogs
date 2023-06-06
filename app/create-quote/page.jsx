e71e872f2fe78ceca9fc7d9323d38e58c53e5b35
Aziz <azizahmed.dev@gmail.com>
2023-06-05 23:38:54 -0400

    added form and submit function for quotes

--- app/create-quote/page.jsx
+++ app/create-quote/page.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+"use client";
+
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+import Form from "@components/Form";
 
 const CreateQuote = () => {
-  return <div>page</div>;
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    quote: "",
+    tag: "",
+  });
+
+  const createQuote = async (e) => {
+    e.preventDefault();
+  };
+  return (
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      setSubmitting={setSubmitting}
+    />
+  );
 };
 
 export default CreateQuote;
